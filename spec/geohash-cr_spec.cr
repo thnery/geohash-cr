f35b5958235a2043f4db7b63ad8216f4e2727ef3
Tácio Nery <tacionery@gmail.com>
2017-11-10 17:48:32 -0300

    Fix geohash-cr.cr Int32 or Nil issue

--- spec/geohash-cr_spec.cr
+++ spec/geohash-cr_spec.cr
@@ -1,9 +1,14 @@
 require "./spec_helper"
 
 describe GeohashCr do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "decodes" do
+    {
+      "c216ne" => [[45.3680419921875, -121.70654296875], [45.37353515625, -121.695556640625]],
+      "C216Ne" => [[45.3680419921875, -121.70654296875], [45.37353515625, -121.695556640625]],
+      "dqcw4"  => [[39.0234375, -76.552734375], [39.0673828125, -76.5087890625]],
+      "DQCW4"  => [[39.0234375, -76.552734375], [39.0673828125, -76.5087890625]],
+    }.each do |hash, latlng|
+      GeohashCr.decode(hash).should eq(latlng)
+    end
   end
 end
--- src/geohash-cr.cr
+++ src/geohash-cr.cr
@@ -1,19 +1,22 @@
 require "./geohash-cr/*"
 
 module GeohashCr
+  extend self
+
   def decode(geohash)
     latlng = [[-90.0, 90.0], [-180.0, 180.0]]
     is_lng = 1
-    geohash.downcase.scan(/./) do |c|
+
+    geohash.downcase.each_char do |c|
       BITS.each do |mask|
-        latlng[is_lng][(BASE32.index(c) & mask)==0 ? 1 : 0] = (latlng[is_lng][0] + latlng[is_lng][1]) / 2
+        latlng[is_lng][(BASE32.index(c).to_s.to_i & mask) == 0 ? 1 : 0] = (latlng[is_lng][0] + latlng[is_lng][1]) / 2
         is_lng ^= 1
       end
     end
     latlng.transpose
   end
 
-  def encode(latitude, longitude, precision=12)
+  def encode(latitude, longitude, precision = 12)
     latlng = [latitude, longitude]
     points = [[-90.0, 90.0], [-180.0, 180.0]]
     is_lng = 1
@@ -22,15 +25,15 @@ module GeohashCr
       5.times do |bit|
         mid = (points[is_lng][0] + points[is_lng][1]) / 2
         points[is_lng][latlng[is_lng] > mid ? 0 : 1] = mid
-        ch |=  BITS[bit] if latlng[is_lng] > mid
+        ch |= BITS[bit] if latlng[is_lng] > mid
         is_lng ^= 1
       end
-      BASE32[ch,1]
+      BASE32[ch, 1]
     }.join
   end
 
   def neighbors(geohash)
-    [[:top, :right], [:right, :bottom], [:bottom, :left], [:left, :top]].map{ |dirs|
+    [[:top, :right], [:right, :bottom], [:bottom, :left], [:left, :top]].map { |dirs|
       point = adjacent(geohash, dirs[0])
       [point, adjacent(point, dirs[1])]
     }.flatten
@@ -38,8 +41,8 @@ module GeohashCr
 
   def parent_neighbors(geohash)
     parent = geohash[0...-1]
-    lat1, lng1 = decode(geohash).reduce { |a,b| [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2]  }
-    lat2, lng2 = decode(parent).reduce { |a,b| [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2]  }
+    lat1, lng1 = decode(geohash).reduce { |a, b| [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2] }
+    lat2, lng2 = decode(parent).reduce { |a, b| [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2] }
 
     top, top_right, right, bottom_right, bottom, bottom_left, left, top_left = neighbors(parent)
 
@@ -50,28 +53,28 @@ module GeohashCr
   end
 
   def adjacent(geohash, dir)
-    base, lastChr = geohash[0...-1], geohash[-1,1]
-    type = (geohash.length % 2)==1 ? :odd : :even
+    base, lastChr = geohash[0...-1], geohash[-1, 1]
+    type = (geohash.length % 2) == 1 ? :odd : :even
     if BORDERS[dir][type].include?(lastChr)
       base = adjacent(base, dir)
     end
-    base + BASE32[NEIGHBORS[dir][type].index(lastChr),1]
+    base + BASE32[NEIGHBORS[dir][type].index(lastChr), 1]
   end
 
-  BITS = [0x10, 0x08, 0x04, 0x02, 0x01]
+  BITS   = [0x10, 0x08, 0x04, 0x02, 0x01]
   BASE32 = "0123456789bcdefghjkmnpqrstuvwxyz"
 
   NEIGHBORS = {
-    right: { even: "bc01fg45238967deuvhjyznpkmstqrwx", odd: "p0r21436x8zb9dcf5h7kjnmqesgutwvy" },
-    left: { even: "238967debc01fg45kmstqrwxuvhjyznp", odd: "14365h7k9dcfesgujnmqp0r2twvyx8zb" },
-    top: { even: "p0r21436x8zb9dcf5h7kjnmqesgutwvy", odd: "bc01fg45238967deuvhjyznpkmstqrwx" },
-    bottom: { even: "14365h7k9dcfesgujnmqp0r2twvyx8zb", odd: "238967debc01fg45kmstqrwxuvhjyznp" }
+    right:  {even: "bc01fg45238967deuvhjyznpkmstqrwx", odd: "p0r21436x8zb9dcf5h7kjnmqesgutwvy"},
+    left:   {even: "238967debc01fg45kmstqrwxuvhjyznp", odd: "14365h7k9dcfesgujnmqp0r2twvyx8zb"},
+    top:    {even: "p0r21436x8zb9dcf5h7kjnmqesgutwvy", odd: "bc01fg45238967deuvhjyznpkmstqrwx"},
+    bottom: {even: "14365h7k9dcfesgujnmqp0r2twvyx8zb", odd: "238967debc01fg45kmstqrwxuvhjyznp"},
   }
 
   BORDERS = {
-    righ: { even: "bcfguvyz", odd: "prxz" },
-    left: { even: "0145hjnp", odd: "028b" },
-    top: { even: "prxz", odd: "bcfguvyz" },
-    bottom: { even: "028b", odd: "0145hjnp" }
+    righ:   {even: "bcfguvyz", odd: "prxz"},
+    left:   {even: "0145hjnp", odd: "028b"},
+    top:    {even: "prxz", odd: "bcfguvyz"},
+    bottom: {even: "028b", odd: "0145hjnp"},
   }
 end
